b99d675b7fe9fa595098d3abf73927e5da253f9c
manoj569 <65048364+manoj569@users.noreply.github.com>
2026-08-06 16:18:45 +0530

    Comment out HostApplicationShutdown class
    Comment out HostApplicationShutdown class and its members.

--- JobPortal.API/Startup/HostApplicationShutdown.cs
+++ JobPortal.API/Startup/HostApplicationShutdown.cs
@@ -1,10 +1,10 @@
-using JobPortal.Application.Abstractions.Authentication;
+//using JobPortal.Application.Abstractions.Authentication;
 
-namespace JobPortal.API.Startup;
+//namespace JobPortal.API.Startup;
 
-public sealed class HostApplicationShutdown(
-    IHostApplicationLifetime hostApplicationLifetime) : IApplicationShutdown
-{
-    public CancellationToken ApplicationStopping =>
-        hostApplicationLifetime.ApplicationStopping;
-}
+//public sealed class HostApplicationShutdown(
+//    IHostApplicationLifetime hostApplicationLifetime) : IApplicationShutdown
+//{
+//    public CancellationToken ApplicationStopping =>
+//        hostApplicationLifetime.ApplicationStopping;
+//}
